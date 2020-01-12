b61e9304035927e23636029670b3c51d438cc9f3
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2020-01-12 18:23:34 +0900

    css.upload

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path 
 from .views import signupfunc, loginfunc, listfunc
-from django.conf import settings #画像の設定
-from django.conf.urls.static import static #画像の設定
 
 urlpatterns = [
     path('signup', signupfunc, name='signup'),
     path('login', loginfunc, name='login'),
     path('list/', listfunc),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #画像の設定
+]
